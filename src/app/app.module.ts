056e6f5d60ace81e792c9c2c363b054f88051f5a
Alex Torres <alfratomo.1983@gmail.com>
2021-12-11 05:15:31 -0300

    se incorpora componente listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
+import { ListadoComponent } from './components/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistroComponent,
-    LoginComponent
+    LoginComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule,
